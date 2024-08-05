a1f4e062ce4a67eb9bd46e21651de88c62845d4f
cabrown24 <calebbrown1231@gmail.com>
2024-08-04 22:00:04 -0500

    adding some javascript validation and css with pseudoclasses

---
+++ script.js
@@ -0,0 +1,17 @@
+const confirmedPassword = document.querySelector('#cpassword');
+
+confirmedPassword.addEventListener('input', () => {
+    console.log("change recognized");
+    const password = document.querySelector('#password');
+    let pass = password.value;
+    if(confirmedPassword.value !== password.value) {
+        confirmedPassword.setCustomValidity("Password does not match");
+    }
+
+    else {
+        confirmedPassword.setCustomValidity("");
+        password.style.border = "yellowgreen 1px solid";
+        confirmedPassword.style.border = "yellowgreen 1px solid";
+    }
+});
+
